6f1c6462c1255cf94ff3df3865b3b39e835b8025
Luca Martinelli <l.martinelli@hotmail.com>
2021-02-28 18:06:28 +0100

    fix(app.ts): port for test

--- airtable/app.ts
+++ airtable/app.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import { Loader } from './loaders';
 import Logger from './loaders/logger';
 
-function normalizePort(val: string | number | undefined, fallback = 3000): number {
+function normalizePort(val: string | number | undefined, fallback = 3333): number {
     if (typeof val === 'string') {
         const port = parseInt(val, 10);
         if (!isNaN(port) && port > 0)
