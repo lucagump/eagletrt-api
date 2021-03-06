40c6c012debba101ccae515535087ed6c0ad7bdc
Luca Martinelli <l.martinelli@hotmail.com>
2021-03-06 12:45:47 +0100

    fix(live): transpile script

--- live/tests/postman/index.ts
+++ live/tests/postman/index.ts
@@ -4,7 +4,7 @@ import { ChildProcess, spawn } from 'child_process';
 
 export default async function() {
     
-    describe('History / Postaman Tests', () => {
+    describe('Live / Postaman Tests', () => {
         let childServer: ChildProcess;
         
         before(async function() {
